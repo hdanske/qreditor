053c70d42926df6c602bcb10344901857693ded9
yprokopenko <yprokopenko@intervale.ru>
2023-01-20 00:28:17 +0300

    w

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,35 +8,42 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class Controller {
     private static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
     @FXML
-    private TextField urlInput;
+    public TextField urlInput;
 
     @FXML
-    private Text resultLinkId;
+    public TextField idInput;
+
+    @FXML
+    private Label resultLinkId;
+
 
     @FXML
     private Button btn;
 
-    @FXML
-    private void initialize (){
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                logger.toString();
-            }
-        });
-    }
+//    @FXML
+//    private void initialize (){
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent event) {
+//                logger.toString();
+//            }
+//        });
+//    }
 
     @FXML
-    void setResultLinkText (){
+    void setResultLinkText() {
         urlInput = new TextField();
-        resultLinkId = new Text();
+//        resultLinkId = new Text();
         urlInput.textProperty().addListener(new ChangeListener<String>() {
 
             @Override
@@ -45,7 +52,31 @@ public class Controller {
                 resultLinkId.setText(newValue);
             }
         });
-   };
+    }
 
+    ;
+
+    public void onClickEvent(MouseEvent mouseEvent) {
+//        Button btn = (Button) mouseEvent.getSource();
+//        resultLinkId.setText(resultLinkId.getText() + btn.getText());
+    }
+
+    public void setValue(String str) {
+//        resultLinkId.setText("123");
+        System.out.println(resultLinkId.getText());
+    }
+
+
+    public void onKeyTyped(KeyEvent keyEvent) {
+        resultLinkId.setText(keyEvent.getCharacter() + urlInput.getText());
+
+
+//        str += keyEvent.getCharacter();
+//        System.out.println(urlInput.getText());
+    }
+
+//    public void addCharacter (String text) {
+//        resultLinkId.
+//    }
 }
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("QR Editor");
         primaryStage.setScene(new Scene(root, 640, 400));
